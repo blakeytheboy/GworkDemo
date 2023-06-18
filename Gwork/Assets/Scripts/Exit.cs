87cd0d42ea67a8fac8e32e06644117dd56dec5a0
Blake Andrews <blakedandrews0@gmail.com>
2023-06-19 00:30:40 +1000

    1 more bug
    try and fix the ending script thingy - room navigation unpack room

--- Gwork/Assets/Scripts/Exit.cs
+++ Gwork/Assets/Scripts/Exit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+// will allow you to see the public varriables in the inspector
 public class Exit
 {
     public string keyString;
