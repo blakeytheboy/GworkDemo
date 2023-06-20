7609e9d8311cb3a5f1d923de69748bdc5134ec3c
Blake Andrews <blakedandrews0@gmail.com>
2023-06-20 23:48:06 +1000

    dialogue!
    works in arrays, clean it up

--- Gwork/Assets/Scripts/Combat/TurnBasedCombat.cs
+++ Gwork/Assets/Scripts/Combat/TurnBasedCombat.cs
@@ -2,23 +2,38 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+[System.Serializable]
+public struct EnemyDialogueLine
+{
+    public int rollValue;       // Roll value at which the dialogue line triggers
+    public string dialogueLine; // Dialogue line spoken by the enemy
+}
+
 public class TurnBasedCombat : MonoBehaviour
 {
-    public int playerMinDamage = 1;  // Minimum damage player's weapon can deal
-    public int playerMaxDamage = 20; // Maximum damage player's weapon can deal
-    public int enemyBaseDamage = 5;  // Base damage of the enemy
-    public int minRoll = 1;          // Minimum roll value (d20)
-    public int maxRoll = 20;         // Maximum roll value (d20)
+    public int playerBaseDamage = 10;   // Base damage player's weapon can deal
+    public int enemyBaseDamage = 5;     // Base damage of the enemy
+    public int MinRoll = 1;       // Minimum roll value for the player and enemy (d20)
+    public int MaxRoll = 20;      // Maximum roll value for the player and enemy (d20)
+    public int playerMinMissRoll = 1;   // Minimum roll value for miss chance (player)
+    public int playerMaxMissRoll = 5;   // Maximum roll value for miss chance (player)
+    public int enemyMinMissRoll = 1;    // Minimum roll value for miss chance (enemy)
+    public int enemyMaxMissRoll = 5;    // Maximum roll value for miss chance (enemy)
+    public int playerMinCriticalRoll = 18;  // Minimum roll value for player's critical hit
+    public int playerMaxCriticalRoll = 20;  // Maximum roll value for player's critical hit
+    public int enemyMinCriticalRoll = 18;   // Minimum roll value for enemy's critical hit
+    public int enemyMaxCriticalRoll = 20;   // Maximum roll value for enemy's critical hit
+    public EnemyDialogueLine[] enemyDialogues; // Array of enemy dialogue lines
 
     public int playerHealth = 100;   // Player's initial health
     public int enemyHealth = 100;    // Enemy's initial health
 
-    public TMP_InputField inputField;   // TMP_InputField for player input
-    public TMP_Text combatLogText;      // TMP_Text for displaying combat log
-    public TMP_Text playerHealthText;   // TMP_Text for displaying player's health
-    public TMP_Text enemyHealthText;    // TMP_Text for displaying enemy's health
+    public TMP_InputField inputField;         // TMP_InputField for player input
+    public TMP_Text combatLogText;            // TMP_Text for displaying combat log
+    public TMP_Text playerHealthText;         // TMP_Text for displaying player's health
+    public TMP_Text enemyHealthText;          // TMP_Text for displaying enemy's health
 
-    public string sceneToLoad;          // Name of the scene to load after battle ends
+    public string sceneToLoad;                // Name of the scene to load after battle ends
 
     private bool isInCombat = true; // Indicates whether the player is currently in combat
     private bool playerTurn = true;  // Indicates whether it's the player's turn
@@ -43,24 +58,24 @@ public class TurnBasedCombat : MonoBehaviour
 
             if (input == "roll")
             {
-                int roll = Random.Range(minRoll, maxRoll + 1);
+                int roll = Random.Range(MinRoll, MaxRoll + 1);
                 inputField.text = ""; // Clear the input field
 
                 combatLogText.text += $"\nPlayer rolls: {roll}";
 
-                if (roll == 1)
+                if (roll >= playerMinMissRoll && roll <= playerMaxMissRoll)
                 {
                     combatLogText.text += "\nYou missed the enemy!";
                 }
-                else if (roll == 20)
+                else if (roll >= playerMinCriticalRoll && roll <= playerMaxCriticalRoll)
                 {
-                    int damage = Random.Range(playerMinDamage, playerMaxDamage + 1) * 2;
+                    int damage = playerBaseDamage * 2;
                     enemyHealth -= damage;
                     combatLogText.text += $"\nCritical hit! You dealt {damage} damage to the enemy.";
                 }
                 else
                 {
-                    int damage = Random.Range(playerMinDamage, playerMaxDamage + 1);
+                    int damage = playerBaseDamage;
                     enemyHealth -= damage;
                     combatLogText.text += $"\nYou dealt {damage} damage to the enemy.";
                 }
@@ -85,14 +100,23 @@ public class TurnBasedCombat : MonoBehaviour
     private void EnemyTurn()
     {
         // Roll the dice for the enemy's attack
-        int roll = Random.Range(minRoll, maxRoll + 1);
+        int roll = Random.Range(MinRoll, MaxRoll + 1);
         combatLogText.text += $"\nEnemy rolls: {roll}";
 
-        if (roll == 1)
+        for (int i = 0; i < enemyDialogues.Length; i++)
+        {
+            if (roll == enemyDialogues[i].rollValue)
+            {
+                combatLogText.text += $"\nEnemy says: {enemyDialogues[i].dialogueLine}";
+                break;
+            }
+        }
+
+        if (roll >= enemyMinMissRoll && roll <= enemyMaxMissRoll)
         {
             combatLogText.text += "\nThe enemy missed you!";
         }
-        else if (roll == 20)
+        else if (roll >= enemyMinCriticalRoll && roll <= enemyMaxCriticalRoll)
         {
             int damage = enemyBaseDamage * 2;
             playerHealth -= damage;
@@ -100,8 +124,9 @@ public class TurnBasedCombat : MonoBehaviour
         }
         else
         {
-            playerHealth -= enemyBaseDamage;
-            combatLogText.text += $"\nThe enemy dealt {enemyBaseDamage} damage to you.";
+            int damage = enemyBaseDamage;
+            playerHealth -= damage;
+            combatLogText.text += $"\nThe enemy dealt {damage} damage to you.";
         }
 
         UpdateHealthTexts();
